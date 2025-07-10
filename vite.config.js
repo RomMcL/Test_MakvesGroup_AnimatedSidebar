aa8ac763a92d7a6c906b58cc95d7c8cfe7fe504f
RomMcL <rommcl@yandex.ru>
2025-07-11 01:56:13 +0300

    Правки
    длинное имя репозитория

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Test_MakvesGroup_AnimatedSidebar/',
+  base: '/Test_MakvesGroup_Sidebar',
 })
